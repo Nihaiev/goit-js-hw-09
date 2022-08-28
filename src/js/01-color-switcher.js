3fe2033e825444bc1d5f1ecf9c183363ea40f0e5
Иван Нигаев <Nihaiev@it.group>
2022-08-28 15:02:26 +0300

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+console.log(startBtn);
+console.log(stopBtn);
 
+const DELAY = 1000;
+let intervalId = null;
+
+startBtn.addEventListener('click', changeBgColor);
+stopBtn.addEventListener('click', stopchangeBgColor);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function changeBgColor() {
+    intervalId = setInterval(() => {
+        console.log('Меняем фон');
+         document.body.style.background = getRandomHexColor();
+      }, DELAY);
+
+      startBtn.setAttribute("disabled", true);
+}
+
+function stopchangeBgColor() {
+    clearInterval(intervalId);
+    startBtn.removeAttribute("disabled");
+}
